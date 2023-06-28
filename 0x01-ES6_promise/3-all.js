f26d97b9f4314c372495b17c79c5098fdb01208b
Mdigo12 <mdigojacob@gmail.com>
2023-06-28 22:04:23 +0300

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
+
+  return Promise.all([photoPromise, userPromise])
+    .then(([photoResults, userResults]) => {
+      console.log(photoResults.body, userResults.firstName, userResults.lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
