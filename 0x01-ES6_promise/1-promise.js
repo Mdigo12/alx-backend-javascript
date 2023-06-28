bdd61cb40d75890a9ccb5187d186321373b55d34
Mdigo12 <mdigojacob@gmail.com>
2023-06-28 20:05:28 +0300

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve(
+        {
+          status: 200,
+          body: 'Success',
+        },
+      );
+    }
+
+    if (success === false) {
+      reject('Error: The fake API is not working currently');
+    }
+  });
+}
