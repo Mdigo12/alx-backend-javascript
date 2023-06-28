c16bbbf1492cb52852883305c898ef7f40cadd6f
Mdigo12 <mdigojacob@gmail.com>
2023-06-28 23:23:06 +0300

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,26 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [];
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
-  promises.push(signUpUser(firstName, lastName));
-  promises.push(uploadPhoto(fileName));
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => {
+      const settledPromises = results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
 
-  const results = [];
-
-  return Promise.allSettled(promises)
-    .then((settledPromises) => {
-      for (let i = 0; i < settledPromises.length; i += 1) {
-        const result = settledPromises[i];
-        const { status } = result;
-        const value = status === 'fulfilled' ? result.value : result.reason;
-
-        results.push({
-          status,
-          value,
-        });
-      }
-
-      return results;
+      return settledPromises;
     });
 }
